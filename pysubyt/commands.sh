e9913cfd6f6596cc8a012aa120ef39a87efdff4e
cedricd <cedricdecruw@gmail.com>
2022-12-20 14:17:42 +0100

    added V2

--- pysubyt/commands.sh
+++ pysubyt/commands.sh
@@ -13,4 +13,15 @@ rm ./output/v1_material_sample_output.ttl
 python -m pysubyt -t ./templates/ -i ./input/EMO_BON_Metadata_Soft_Sediment_BPNS_Belgium\ -\ sampling.csv -n v1_material_sample.ttl -o ./output/v1_material_sample_output.ttl -s col_md ./input/EMO\ BON\ logsheet\ metadata\ -\ For\ LOD\ project.csv
 
 #concatinate ./output/prefixes.ttl with all v1 files into one ttl file
-cat ./output/prefixes.ttl ./output/v1_observatory_output.ttl ./output/v1_event_output.ttl ./output/v1_material_sample_output.ttl > ./output/v1_output.ttl
+cat ./output/prefixes.ttl ./output/v1_project.ttl ./output/v1_observatory_output.ttl ./output/cleaned_v1_event_output.ttl ./output/v1_material_sample_output.ttl > ./output/v1_combined_output.ttl
+
+#v2 material samples
+rm ./output/v2_material_sample_output.ttl
+python -m pysubyt -t ./templates/ -i ./input/EMO_BON_Metadata_Soft_Sediment_BPNS_Belgium\ -\ sampling.csv -n v2_material_sample.ttl -o ./output/v2_material_sample_output.ttl
+
+#v2 environmental measurements
+rm ./output/v2_environmental_measurement_output.ttl
+python -m pysubyt -t ./templates/ -i ./input/EMO_BON_Metadata_Soft_Sediment_BPNS_Belgium\ -\ sampling.csv -n v2_environmental_measurement.ttl -o ./output/v2_environmental_measurement_output.ttl -s col_md ./input/EMO\ BON\ logsheet\ metadata\ -\ For\ LOD\ project.csv
+
+#concatinate ./output/prefixes.ttl with all v2 files into one ttl file with an empty line as seperator
+cat ./output/prefixes.ttl ./output/v1_project.ttl ./output/v1_observatory_output.ttl ./output/cleaned_v1_event_output.ttl  ./output/v2_material_sample_output.ttl ./output/v2_environmental_measurement_output.ttl > ./output/v2_combined_output.ttl
